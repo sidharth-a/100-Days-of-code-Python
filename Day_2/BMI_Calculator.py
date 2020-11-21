266893237201e44626f04ecdc5c2ab1f9c203097
sidharth-a <56468787+sidharth-a@users.noreply.github.com>
2020-11-21 16:00:01 +0530

    BMI_Calculator.py

---
+++ Day_2/BMI_Calculator.py
@@ -0,0 +1,9 @@
+height = input("enter your height in m: ")
+weight = input("enter your weight in kg: ")
+
+print(int((int(weight))/((float(height))**2)))
+
+# h = float(height)
+# w = int(weight)
+# bmi = w/(h**2)
+# print(int(bmi))
