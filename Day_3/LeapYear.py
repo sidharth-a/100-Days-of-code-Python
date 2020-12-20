428ca2af59f628cb9387f351eae8800280e9c5a3
sidharth-a <56468787+sidharth-a@users.noreply.github.com>
2020-12-20 22:47:35 +0530

    LeapYear.py
    Leap Year Calculator

---
+++ Day_3/LeapYear.py
@@ -0,0 +1,14 @@
+
+print("Welcome to Leap Year Calculator :)")
+year = int(input("Which year do you want to check? "))
+
+if year%4==0:
+  if year%100==0:
+    if year%400==0:
+      print("Leap Year")
+    else:
+      print("Not Leap Year")
+  else:
+    print("Leap Year")
+else:
+  print("Not Leap Year")
